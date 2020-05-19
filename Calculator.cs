b8d08c7ed9f3614e9d5bf862fb159230bd79889a
sofiakhatoon <skhatoon@gmail.com>
2020-05-19 14:05:17 -0500

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   Calculator.cs 	new file:   c#event.csproj 	new file:   program.cs

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace c_event
+{
+    public class Calculator
+    {
+        private ManualResetEventSlim _mEvent;
+
+        public int Result { get; private set; }
+
+        public Calculator(ManualResetEventSlim ev)
+        {
+            _mEvent = ev;
+        }
+
+        public void Calculation(int x, int y)
+        {
+            WriteLine($"Task {Task.CurrentId} starts addition after 3 sec for " +x +" and " + y);
+            Task.Delay(new Random().Next(3000)).Wait();
+            Result = x + y;
+
+            // signal the event-completed!
+            WriteLine($"calculation {Task.CurrentId} is ready");
+            _mEvent.Set();
+        }
+    }
+
+}
